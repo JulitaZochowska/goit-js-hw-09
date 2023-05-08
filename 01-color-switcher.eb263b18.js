c1024bfaf0962a13a389fd0f6c0d94979bf8db77
JulitaZochowska <119934821+JulitaZochowska@users.noreply.github.com>
2023-05-08 19:07:41 +0000

    Deploying to gh-pages from @ JulitaZochowska/goit-js-hw-09@a1432a5b3d64e10bf522906cc95a65d195ed82b8 🚀

---
+++ 01-color-switcher.eb263b18.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=null;var o=function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))};t.addEventListener("click",(function(){n=setInterval(o,1e3),t.setAttribute("disabled","")})),e.addEventListener("click",(function(){clearInterval(n),t.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.eb263b18.js.map
