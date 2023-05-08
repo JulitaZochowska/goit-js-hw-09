a1432a5b3d64e10bf522906cc95a65d195ed82b8
JulitaZochowska <julita.zochowska@outlook.com>
2023-05-08 21:07:14 +0200

    Exercise 1-done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+let timerID = null;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const intervalFunction = () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+};
+const onClick = () => {
+  timerID = setInterval(intervalFunction, 1000);
+  startBtn.setAttribute('disabled', '');
+};
+const offClick = () => {
+  clearInterval(timerID);
+  startBtn.removeAttribute('disabled');
+};
+startBtn.addEventListener('click', onClick);
+stopBtn.addEventListener('click', offClick);
